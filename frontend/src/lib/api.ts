960098948e50af80adb3a7de8a29404f0fe482a6
kenji.dev <hello.kenji.dev@gmail.com>
2025-07-12 18:12:26 +0800

    feat: did changes - working hamburger menu (alerts to be worked later on) - prepped api thru api.ts

---
+++ frontend/src/lib/api.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const api = axios.create({
+  // change this to BACKEND_URL if data is already present. else use placeholder MOCKAPI_URL
+  baseURL: process.env.BACKEND_URL,
+});
+
+export default api;
--- frontend/src/routes/Layout.tsx
+++ frontend/src/routes/Layout.tsx
@@ -1,9 +1,9 @@
-import { Navbar } from "@/components/ui/Navbar";
+import { MobileNavbar } from "@/components/ui/MobileNavbar";
 import { Outlet } from "react-router";
 export const Layout = () => {
   return (
     <div className="flex flex-col max-w-2xl w-full m-auto py-4">
-      <Navbar />
+      <MobileNavbar />
       <main className="flex-col justify-between py-8">
         <Outlet />
       </main>
