b19a61f12a47b7c2cb5dbfacfbff6796c5c0aec3
kenji.dev <hello.kenji.dev@gmail.com>
2025-08-20 19:33:41 +0800

    chore: major revisions - productAPI calls - installed tanstack query, forms, router

---
+++ frontend/apps/web/src/types/index.ts
@@ -0,0 +1,34 @@
+// Product
+export interface Product {}
+
+export interface ProductDTO extends Product {}
+
+export interface ProductFormValues {}
+
+// User
+export interface User {}
+
+export interface UserDTO extends User {}
+
+export interface UserFormValues {}
+
+// Customer
+export interface Customer {}
+
+export interface CustomerDTO extends Customer {}
+
+export interface CustomerFormValues {}
+
+// Farmer
+export interface Farmer {}
+
+export interface FarmerDTO extends Farmer {}
+
+export interface FarmerFormValues {}
+
+// Order
+export interface Order {}
+
+export interface OrderDTO extends Order {}
+
+export interface OrderFormValues {}
--- frontend/src/lib/api.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  // change this to BACKEND_URL if data is already present. else use placeholder MOCKAPI_URL
-  baseURL: process.env.MOCKAPI_URL,
-});
-
-export default api;
